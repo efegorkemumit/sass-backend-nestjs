32131ae3d7b401b0154794b212935b45190773fc
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2026-01-01 22:55:43 +0300

    NestJS Eğitimi #5 – SaaS Backend için Modüler Dosya Yapısı ve Proje İskeleti

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,20 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './config/env.schema';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { OrganizationsModule } from './modules/organizations/organizations.module';
+import { MembershipsModule } from './modules/memberships/memberships.module';
+import { ServicesModule } from './modules/services/services.module';
+import { AvailabilityModule } from './modules/availability/availability.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { AuditModule } from './modules/audit/audit.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal:true,
     validate:(env)=>envSchema.parse(env)
-  })],
+  }), AuthModule, OrganizationsModule, MembershipsModule, ServicesModule, AvailabilityModule, BookingsModule, AuditModule, HealthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
